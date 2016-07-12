c4cdb160fbe15c770d75d812c6b2249767861c9f
safooray <safoora.yousefi1192@gmail.com>
2016-07-12 11:02:44 -0400

    Update Bayesian_Optimization.py

--- Bayesian_Optimization.py
+++ Bayesian_Optimization.py
@@ -2,46 +2,52 @@
 """
 Created on Sat Apr  2 22:17:31 2016
 
-@author: Safoora
+@author: Ayine
 """
-
+import sys
+sys.path.append('./../')
 import bayesopt
 import numpy as np
 from time import clock
-from BayesOpt_costfunc import bayesopt_costfunc
+from CostFunction import cost_func
 import theano
 
-def tune(nonlin):
+def tune():
     params = {}
-    params['n_iterations'] = 1000
+    params['n_iterations'] = 200
     params['n_iter_relearn'] = 1
-    params['n_init_samples'] = 2
+    params['n_init_samples'] = 5
     
     print "*** Model Selection with BayesOpt ***"
-    
-    n = 3                   # n dimensions
-    #lb = np.array([1,  10,  .0])
-    #ub = np.array([10, 300, .9])
+    n = 6                # n dimensions
+# params: #layer, width, dropout, L2reg, nonlinearity 
+    lb = np.array([1,  10, .0, 0, 0, 0])
+    ub = np.array([5, 100, .9, 1, 1, 1])
 
     start = clock()
-    #mvalue, x_out, error = bayesopt.optimize(bayesopt_costfunc, n, lb, ub, params)
-    layers = [1, 3, 5, 7, 9, 10]
-    hsizes = [10, 50, 100, 150, 200, 300]
-    drates = [0.0, .1, .3, .5, .7, .9]
-    x_set = []
-    for l in layers:
-	for h in hsizes:
-            for d in drates:
-	        x_set.append([l, h, d])
-    x_set = np.array(x_set, dtype=float)
+    mvalue, x_out, error = bayesopt.optimize(cost_func, n, lb, ub, params)
+    #layers = [1, 3, 5, 7, 9, 10]
+    #hsizes = [10, 50, 100, 150, 200, 300]
+    #drates = [0.0, .1, .3, .5, .7, .9]
+    #layers = [1, 5, 10]
+    #hsizes = [10, 100, 1000, 10000]
+    #drates = [.0, .1, .5, .9]
+	#reg = []
+    #x_set = []
+    #for l in layers:
+	#for h in hsizes:
+    #        for d in drates:
+	#        x_set.append([l, h, d])
+    #x_set = np.array(x_set, dtype=float)
     #x_set = np.array([[1, 3, 5, 7, 9, 10],\
     #                  [10, 50, 100, 150, 200, 300],\
     #                  [0, 1, 3, 5, 7, 9]], dtype=float).transpose()
-    print x_set.shape
+    #print x_set.shape
 
-    print x_set
-    mvalue, x_out, error = bayesopt.optimize_discrete(bayesopt_costfunc, x_set, params)
+    #print x_set
+    #mvalue, x_out, error = bayesopt.optimize_discrete(cost_func, x_set, params)
     print "Result", mvalue, "at", x_out
     print "Running time:", clock() - start, "seconds"
     return mvalue, x_out, error
-    
+if __name__=='__main__':
+	tune()     
