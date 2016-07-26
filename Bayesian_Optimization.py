c975e3df86a3ee5fa2706059d98092089a60f8cf
safooray <safoora.yousefi1192@gmail.com>
2016-07-26 18:50:35 -0400

    Update Bayesian_Optimization.py

--- Bayesian_Optimization.py
+++ Bayesian_Optimization.py
@@ -9,23 +9,23 @@ sys.path.append('./../')
 import bayesopt
 import numpy as np
 from time import clock
-from CostFunction import cost_func
+from CostFunction import cost_func, aggr_st_cost_func, st_cost_func, panorg_cost_func
 import theano
 
-def tune():
+def tune(i):
     params = {}
-    params['n_iterations'] = 200
+    params['n_iterations'] = 100
     params['n_iter_relearn'] = 1
-    params['n_init_samples'] = 5
+    params['n_init_samples'] = 2
     
     print "*** Model Selection with BayesOpt ***"
-    n = 6                # n dimensions
+    n = 5                # n dimensions
 # params: #layer, width, dropout, L2reg, nonlinearity 
-    lb = np.array([1,  10, .0, 0, 0, 0])
-    ub = np.array([5, 100, .9, 1, 1, 1])
+    lb = np.array([1,  10, .0, 0, i])
+    ub = np.array([5, 500, .9, 1, i+.5])
 
     start = clock()
-    mvalue, x_out, error = bayesopt.optimize(cost_func, n, lb, ub, params)
+    mvalue, x_out, error = bayesopt.optimize(panorg_cost_func, n, lb, ub, params)
     #layers = [1, 3, 5, 7, 9, 10]
     #hsizes = [10, 50, 100, 150, 200, 300]
     #drates = [0.0, .1, .3, .5, .7, .9]
@@ -50,4 +50,4 @@ def tune():
     print "Running time:", clock() - start, "seconds"
     return mvalue, x_out, error
 if __name__=='__main__':
-	tune()     
+	tune(0)     
