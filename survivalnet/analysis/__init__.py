3c8acda896d9a900d1a7982919b2ef5ae606ed49
Lee Cooper <cooperle@gmail.com>
2016-08-17 23:53:47 -0400

    Added GSEA-related functions.

--- survivalnet/analysis/__init__.py
+++ survivalnet/analysis/__init__.py
@@ -1,6 +1,9 @@
 from .RiskCohort import RiskCohort
 
 # must be imported after RiskCohort
+from .ReadGMT import ReadGMT
+from .RiskCluster import RiskCluster
+from .SSGSEA import SSGSEA
 from .Visualize import PairScatter
 from .Visualize import RankedBar
 from .Visualize import RankedBox
@@ -8,6 +11,9 @@ from .Visualize import Visualize
 
 # list functions and classes available for public use
 __all__ = (
+	'ReadGMT',
+	'RiskCluster',
+	'SSGSEA',
     'PairScatter',
     'RankedBar',
     'RankedBox',
