47502bae5e8586d8520e43d5ec94c3b81f5d413e
syousefi <safoora.yousefi1192@gmail.com>
2016-10-12 13:49:24 -0400

    removed redundant forward pass

--- survivalnet/model/DropoutHiddenLayer.py
+++ survivalnet/model/DropoutHiddenLayer.py
@@ -20,9 +20,8 @@ def _dropout_from_layer(rng, layer, p):
     p is the probablity of dropping a unit
     """
     srng = T.shared_randomstreams.RandomStreams(rng.randint(999999))
-    mask = theano.printing.Print("droping out =")(srng.binomial(n=1, p=1-p, size=layer.shape))
+    mask = srng.binomial(n=1, p=1-p, size=layer.shape)
     # p=1-p because 1's indicate keep and p is prob of dropping
-    # check = theano.printing.Print('mask')(mask)
     # The cast is important because
     # int * float32 = float64 which pulls things off the gpu
     output = layer * T.cast(mask, theano.config.floatX)
