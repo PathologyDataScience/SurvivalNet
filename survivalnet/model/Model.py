3de20ae5abedddac75ac1792d9f64006cade08c1
safooray <safoora.yousefi1192@gmail.com>
2017-08-31 12:24:40 -0400

    model cleanup

--- survivalnet/model/Model.py
+++ survivalnet/model/Model.py
@@ -1,6 +1,4 @@
-
 import numpy
-
 import theano
 import theano.tensor as T
 from theano.tensor.shared_randomstreams import RandomStreams
@@ -10,7 +8,6 @@ from .HiddenLayer import HiddenLayer
 from .DropoutHiddenLayer import DropoutHiddenLayer
 from .SparseDenoisingAutoencoder import SparseDenoisingAutoencoder as dA
 from survivalnet.optimization import Optimization as Opt
-from theano.compile.nanguardmode import NanGuardMode
 
 
 class Model(object):
@@ -55,26 +52,30 @@ class Model(object):
         :param non_lin: nonlinear activation function used in all layers
                 
         """
-        # Initialize parameters
-        self.hidden_layers = []; self.dA_layers = []; self.params = []; self.dropout_masks = []
-        self.n_layers = len(hidden_layers_sizes); self.L1 = 0; self.L2_sqr = 0; self.n_hidden = hidden_layers_sizes[0]
+        # Initializes parameters.
+        self.hidden_layers = [];
+		self.dA_layers = []; 
+		self.params = []; 
+		self.dropout_masks = []
+        self.n_layers = len(hidden_layers_sizes); 
+		self.L1 = 0; 
+		self.L2_sqr = 0; 
+		self.n_hidden = hidden_layers_sizes[0]
         if not theano_rng:
             theano_rng = RandomStreams(numpy_rng.randint(2 ** 30))
 
-        # Allocate symbolic variables for the data
-        self.x = T.matrix('x', dtype='float32')                                                 # Expression data
-        self.o = T.ivector('o')                                                                 # Observed death or not, 1 is death, 0 is right censored
-        self.AtRisk = T.ivector('AtRisk')        
-        self.is_train = T.iscalar('is_train')                                                   # Indicator of training/testing used in dropout
-        self.masks = [T.lmatrix('mask_' + str(i)) for i in range(self.n_layers)]
+        # Allocates symbolic variables for the data.
+        self.x = T.matrix('x', dtype='float32')
+		self.o = T.ivector('o')  
+		self.at_risk = T.ivector('at_risk')     
+        self.is_train = T.iscalar('is_train')
+		self.masks = [T.lmatrix('mask_' + str(i)) for i in range(self.n_layers)]
 
-        # Simple cox regression with no hidden layers
+        # Linear cox regression with no hidden layers.
         if self.n_layers == 0:                              
-            self.riskLayer = RiskLayer(input=self.x, n_in=n_ins, n_out=n_outs, rng = numpy_rng)
+            self.risk_layer = RiskLayer(input=self.x, n_in=n_ins, n_out=n_outs, rng = numpy_rng)
         else:    
-            # Construct the intermediate layer
-            # the size of the input is either the number of hidden units of
-            # the layer below or the input size if we are on the first layer
+            # Constructs the intermediate layers.
             for i in xrange(self.n_layers):     
                 if i == 0:
                     input_size = n_ins
@@ -83,27 +84,27 @@ class Model(object):
                     input_size = hidden_layers_sizes[i - 1]
                     layer_input = self.hidden_layers[-1].output
 
-                # The input to this layer is either the activation of the hidden
-                # layer below or the input of the Model if you are on the first layer
-                hidden_layer = DropoutHiddenLayer(rng=numpy_rng,
-                                            input=layer_input,
-                                            n_in=input_size,
-                                            n_out=hidden_layers_sizes[i],
-                                            activation=non_lin,
-                                            dropout_rate=dropout_rate,
-                                            is_train=self.is_train,
-                                            mask = self.masks[i]) \
-                    if dropout_rate > 0 else HiddenLayer(rng=numpy_rng,
-                                            input=layer_input,
-                                            n_in=input_size,
-                                            n_out=hidden_layers_sizes[i],
-                                            activation=non_lin)
+				if dropout_rate > 0:
+					hidden_layer = DropoutHiddenLayer(rng=numpy_rng,
+													input=layer_input,
+													n_in=input_size,
+													n_out=hidden_layers_sizes[i],
+													activation=non_lin,
+													dropout_rate=dropout_rate,
+													is_train=self.is_train,
+													mask = self.masks[i])
+				else:
+					hidden_layer = HiddenLayer(rng=numpy_rng,
+											input=layer_input,
+											n_in=input_size,
+											n_out=hidden_layers_sizes[i],
+											activation=non_lin)
 
-                # Add the layer to our stack of layers
+                # Adds the layer to the stack of layers.
                 self.hidden_layers.append(hidden_layer)
                 self.params.extend(hidden_layer.params)
     
-                # Construct a denoising autoencoder that shares weights with this layer
+                # Constructs an autoencoder that shares weights with this layer.
                 dA_layer = dA(numpy_rng=numpy_rng,
                               theano_rng=theano_rng,
                               input=layer_input,
@@ -117,19 +118,16 @@ class Model(object):
         self.L1 += abs(hidden_layer.W).sum()
         self.L2_sqr += (hidden_layer.W ** 2).sum()
 
-        # We now need to add a risk prediction layer on top of the stack
-        self.riskLayer = RiskLayer(
-            input=self.hidden_layers[-1].output,
-            n_in=hidden_layers_sizes[-1],
-            n_out=n_outs,
-            rng = numpy_rng)
+        # Adds a risk prediction layer on top of the stack.
+        self.risk_layer = RiskLayer(input=self.hidden_layers[-1].output,
+									n_in=hidden_layers_sizes[-1],
+									n_out=n_outs,
+									rng = numpy_rng)
 
-        self.L1 += abs(self.riskLayer.W).sum()
-        self.L2_sqr += (self.riskLayer.W ** 2).sum()
-        self.L1 *= lambda1
-        self.L2_sqr *= lambda2
-        self.params.extend(self.riskLayer.params)
-        #cost = self.riskLayer.cost + self.L1 + self.L2_sqr
+        self.L1 += abs(self.risk_layer.W).sum()
+        self.L2_sqr += (self.risk_layer.W ** 2).sum()
+        self.params.extend(self.risk_layer.params)
+		self.regularizers = lambda1 * self.L1 + lambda2 * self.L2_sqr
         
     def pretraining_functions(self, pretrain_x, batch_size):
         index = T.lscalar('index')                  # index to a minibatch
@@ -165,7 +163,6 @@ class Model(object):
                     self.is_train: is_train
                 }
             )
-            # append `fn` to the list of functions
             pretrain_fns.append(fn)
 
         return pretrain_fns
@@ -174,32 +171,35 @@ class Model(object):
         
         is_train = T.iscalar('is_train')
         X = T.matrix('X', dtype='float32')
-        AtRisk = T.ivector('AtRisk')
-        Observed = T.ivector('Observed')
-        #call the optimization function
+        at_risk = T.ivector('at_risk')
+        observed = T.ivector('observed')
         opt = Opt()
         
         test = theano.function(
             on_unused_input='ignore',
-            inputs=[X, Observed, AtRisk, is_train] + self.masks,
-            outputs=[self.riskLayer.cost(self.o, self.AtRisk), self.riskLayer.output, self.riskLayer.input],
+            inputs=[X, observed, at_risk, is_train] + self.masks,
+            outputs=[self.risk_layer.cost(self.o, self.at_risk),
+					 self.risk_layer.output, self.risk_layer.input],
             givens={
                 self.x: X,
-                self.o: Observed,
-                self.AtRisk: AtRisk,
+                self.o: observed,
+                self.at_risk: at_risk,
                 self.is_train:is_train
             },
             name='test'
         )
         train = theano.function(
             on_unused_input='ignore',
-            inputs=[X, Observed, AtRisk, is_train] + self.masks,
-            outputs=[self.riskLayer.cost(self.o, self.AtRisk), self.riskLayer.output, self.riskLayer.input],
-            updates=opt.SGD(self.riskLayer.cost(self.o, self.AtRisk) - self.L1 - self.L2_sqr, self.params, learning_rate),
+            inputs=[X, observed, at_risk, is_train] + self.masks,
+            outputs=[self.risk_layer.cost(self.o, self.at_risk), 
+					 self.risk_layer.output, self.risk_layer.input],
+            updates=opt.SGD(
+					self.risk_layer.cost(self.o, self.at_risk)-self.regularizers,
+					self.params, learning_rate),
             givens={
                 self.x: X,
-                self.o: Observed,
-                self.AtRisk: AtRisk,
+                self.o: observed,
+                self.at_risk: at_risk,
                 self.is_train:is_train
             },
             name='train'
@@ -209,7 +209,7 @@ class Model(object):
     def reset_weight(self, params):
         for i in xrange(self.n_layers):
             self.hidden_layers[i].reset_weight((params[2*i], params[2*i+1]))
-        self.riskLayer.reset_weight(params[-1])
+        self.risk_layer.reset_weight(params[-1])
        
     def reset_weight_by_rate(self, rate):
         for i in xrange(self.n_layers):
