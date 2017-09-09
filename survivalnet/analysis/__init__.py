0cf354dc07621b4fbbe42913566f88b1c4604ddb
safooray <safoora.yousefi1192@gmail.com>
2017-09-08 20:29:22 -0400

    fixed analysis init and function call in ModelAnalysis

--- survivalnet/analysis/__init__.py
+++ survivalnet/analysis/__init__.py
@@ -3,15 +3,14 @@ from .RiskCohort import RiskCohort
 # must be imported after RiskCohort
 from .FeatureAnalysis import FeatureAnalysis
 from .PathwayAnalysis import PathwayAnalysis
-from .Visualize import KMPlots
-from .Visualize import PairScatter
-from .Visualize import RankedBar
-from .Visualize import RankedBox
+from .Visualization import KMPlots
+from .Visualization import PairScatter
+from .Visualization import RankedBar
+from .Visualization import RankedBox
 from .WriteGCT import WriteGCT
 from .WriteRNK import WriteRNK
 from .ReadGMT import ReadGMT
 from .RiskCluster import RiskCluster
-from .SSGSEA import SSGSEA
 
 
 # list functions and classes available for public use
